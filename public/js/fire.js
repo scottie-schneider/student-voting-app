9dd3e7468c5738aa7b1167f2a12aa9f8bb28e43f
Scottie Schneider <scott@laclave.co>
2017-09-24 16:24:40 -0500

    adding firebase

---
+++ public/js/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+var config = {
+  apiKey: "AIzaSyC82dkxLCI9vAsflgcg8oFJSz_IC3N7pyg",
+  authDomain: "class-grader-inventive-a7114.firebaseapp.com",
+  databaseURL: "https://class-grader-inventive-a7114.firebaseio.com",
+  projectId: "class-grader-inventive-a7114",
+  storageBucket: "class-grader-inventive-a7114.appspot.com",
+  messagingSenderId: "13741523978"
+};
+var fire firebase.initializeApp(config);
+export default fire;
